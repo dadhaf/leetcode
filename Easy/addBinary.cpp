e687240c8f542865e295b694624d771b20add2bc
INFINEON\hafiz <hafiz.external@infineon.com>
2024-09-06 17:54:57 +0200

    commit to save

--- Easy/addBinary.cpp
+++ Easy/addBinary.cpp
@@ -8,49 +8,89 @@ public:
 
 
     string addBinary(string a, string b) {
-        unsigned long long vala = 0, valb = 0, valc = 0;// = text2dec(a) + text2dec(b);
-        int len = a.length();
-        for(int i = len-1;i>=0;i--)
-        {
-            
-            if(a[i] == '1')
-            {
-                vala += (1<<(len - i - 1));
-            }
-        }
-
-        len = b.length();
-        for(int i = len-1;i>=0;i--)
-        {
-            
-            if(b[i] == '1')
-            {
-                valb += (1<<(len - i - 1));
-            }
-        }
-
-        valc = vala + valb;
-
+        int la = a.size();
+        int lb = b.size();
+        int carry = 0;
         string s = "";
-        while(valc>1)
+        int len = (la > lb) ? lb : la;
+        int maska = (la > lb) ? (la - lb) : 0;
+        int maskb = (la > lb) ? 0 : (lb - la);
+        // int s[(const int)len];
+        for (int i = len - 1; i >=0; i--)
         {
-            if(valc%2 == 0)
+            if(b[i + maskb] == '0' && a[i + maska] == '0'  )
             {
-                s.insert(0,"0");
-            } else
+                if (carry == 1)
+                {
+                    s.insert(0,"1");
+                } else 
+                {
+                    s.insert(0,"0");
+                }
+                
+            } else if(b[i + maskb] == '1' && a[i + maska] == '0'  )
             {
-                s.insert(0,"1");
+                if (carry == 1)
+                {
+                    s.insert(0,"0");
+                    carry = 1;
+                } else 
+                {
+                    s.insert(0,"1");
+                }
+            } else if (b[i + maskb] == '0' && a[i + maska] == '1'  )
+            {
+                if (carry == 1)
+                {
+                    s.insert(0,"0");
+                    carry = 1;
+                } else 
+                {
+                    s.insert(0,"1");
+                    carry = 1;
+                }
+            } else if (b[i + maskb] == '1' && a[i + maska] == '1'  )
+            {
+                if (carry == 1)
+                {
+                    s.insert(0,"1");
+                } else 
+                {
+                    s.insert(0,"0");
+                }
+                carry = 1;
+            } 
+            
+        }
+        if (carry == 1)
+        {
+            if(la > lb)
+            {
+                if(a[len] == '0')
+                {
+                    s.insert(0,"1");
+                } else
+                {
+                    s.insert(0, a.substr(len,la-len));
+                }
+
+                
+            } else if(lb > la)
+            {
+                if(b[len] == '0')
+                {
+                    s.insert(0,"1");
+                } else
+                {
+                    s.insert(0, a.substr(len,lb-len-1));
+                }
+
+                
             }
-            valc=valc/2;
+
         }
 
-        if(valc==1)
-        {
-            s.insert(0,"1");
-        } else
-        {
-            s.insert(0,"0");
-        }
+
 
         return s;
         
@@ -62,7 +102,7 @@ int main()
 {
 
     Solution sol;
-
+    cout << sol.addBinary("11", "1") << endl;
     cout << sol.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011") << endl;
 
     return 0;

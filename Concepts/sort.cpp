998ac9fb261b42f9d178ee51ecef36d2844ed0e7
INFINEON\hafiz <hafiz.external@infineon.com>
2024-09-12 21:53:51 +0200

    insertion sort implemented

--- Concepts/sort.cpp
+++ Concepts/sort.cpp
@@ -22,6 +22,32 @@ public:
             }
         }
     }
+
+
+    void insertionSort(int* nums1, int size)
+    {
+        
+        for(int temp, i=1;i<size;i++)
+        {
+            temp = nums1[i];
+            for(int j=i-1;j>=0;j--)
+            {
+                if(nums1[j]>temp)
+                {
+                    nums1[j+1] = nums1[j];
+                    nums1[j] = temp;
+                } else 
+                {
+                    break;
+                }
+            }
+
+
+        }
+
+
+    } 
+
 };
 
 
@@ -35,6 +61,12 @@ int main()
 
     sol.bubbleSort(a, l);
        for(auto i: a)
+        std::cout << i << std::endl;
+
+    int b[] = {6,2,5,7,1,3,4,8,9,0};
+    l = sizeof(b)/sizeof(b[0]);
+    sol.insertionSort(b, l);
+    for(auto i:b)
         std::cout << i << std::endl;
 
 

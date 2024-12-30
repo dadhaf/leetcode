cc55495a47f337119d93e30e1aec0938e94ef7b1
Adeeb Hafiz <29689720+dadhaf@users.noreply.github.com>
2024-12-30 17:20:21 +0100

    Implementation of inserting element in a linked list

--- Concepts/LinkedList/SinglyNode.cpp
+++ Concepts/LinkedList/SinglyNode.cpp
@@ -16,10 +16,63 @@ public:
     
 };
 
+void insertAtHead(SinglyNode* &node, int data);
+void insertAtHead(SinglyNode* &head, int data){
+    SinglyNode* node = new SinglyNode(data);
+    node->val = data;
+    node->next = head;
+    head = node;
+}
+
+void insertAtLast(SinglyNode* &node, int data);
+void insertAtLast(SinglyNode* &node, int data){
+    SinglyNode* new_node = new SinglyNode(data);
+    SinglyNode* temp = node;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new_node;
+    
+}
+
+void insertAtPosition(SinglyNode* &node, int data, int position);
+void insertAtPosition(SinglyNode* &node, int data, int position){
+    SinglyNode* new_node = new SinglyNode(data);
+    SinglyNode* temp = node;
+    if(position==0)
+    {
+        insertAtHead(node, data);
+        return;
+    }
+    int curr_pos = 0;
+    /* traverse till position - 1 */
+    while(curr_pos < position-1)
+    {
+        temp = temp->next;
+        curr_pos++;
+    }
+    new_node->next = temp->next;
+    temp->next = new_node;
+}
+
+void display(SinglyNode* node);
+void display(SinglyNode* node){
+    SinglyNode* temp = node; /* Pointer to traverse the linked list in forward direction */
+    while(temp!=NULL){
+        cout << temp->val << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
 
 int main(){
-    SinglyNode* node = new SinglyNode(1);
-    cout << "Value: " << node->val << "\nNext Node: " << node->next << endl;
-
+    SinglyNode* node = NULL;
+    insertAtHead(node, 5);
+    display(node);
+    insertAtLast(node,18);
+    display(node);
+    insertAtPosition(node, 12, 1);
+    display(node);
     return 0;
 }
